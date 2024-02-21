e67cd8bb87ee2a7268606166e5f52f6f86d3a7c6
Clément POIRET <poiret.clement@outlook.fr>
2024-02-21 09:01:17 +0100

    feat: from lvim to nvchad

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -4,6 +4,10 @@ local M = {}
 M.ui = {
   theme = 'catppuccin',
   theme_toggle = {'catppuccin', 'github_light'},
+
+  nvdash = {
+    load_on_startup = true,
+  }
 }
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
